da58a4a08b66c2df52b0c07baf75819499981466
PhelixTaken <thegggameyt@gmail.com>
2022-11-09 16:36:50 +0100

    Made a start with drawing the line graph

--- app/src/main/java/nl/hva/vuwearable/ui/chart/ChartFragment.kt
+++ app/src/main/java/nl/hva/vuwearable/ui/chart/ChartFragment.kt
@@ -6,11 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import nl.hva.vuwearable.databinding.FragmentChartBinding
 
 class ChartFragment : Fragment() {
 
     private var _binding: FragmentChartBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -26,7 +34,56 @@ class ChartFragment : Fragment() {
         _binding = FragmentChartBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        setupLineChart()
+
+        setDataToLines()
+
         return root
+    }
+
+    private fun setupLineChart() {
+        with(binding.lineChart) {
+            animateX(1200, Easing.EaseInSine)
+            description.isEnabled = false
+            xAxis.setDrawGridLines(false)
+            xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+            axisRight.isEnabled = false
+            extraRightOffset = 30f
+
+            legend.orientation = Legend.LegendOrientation.VERTICAL
+            legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+            legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+            legend.textSize = 15F
+            legend.form = Legend.LegendForm.LINE
+        }
+    }
+
+    private fun setDataToLines() {
+        val first = LineDataSet(getValues(), "Some Data")
+        first.apply {
+            lineWidth = 3f
+        }
+
+        val dataSet = mutableListOf<ILineDataSet>()
+        dataSet.add(first)
+
+        val lineData = LineData(dataSet)
+        binding.lineChart.data = lineData
+
+        binding.lineChart.invalidate()
+    }
+
+    private fun getValues(): List<Entry> {
+        return listOf(
+            Entry(0f, 13f),
+            Entry(1f, 23F),
+            Entry(2f, 18f),
+            Entry(3f, 3f),
+            Entry(4f, 0f),
+            Entry(5f, 7f),
+            Entry(6f, 18f),
+        )
     }
 
     override fun onDestroyView() {
