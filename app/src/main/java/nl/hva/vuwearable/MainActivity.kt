e0e67ced7f966f626dea000346994680275bc3c1
LorenzoBindemann <2075898@talnet.nl>
2022-11-15 12:51:32 +0100

    Login: added login dialog to the screen

--- app/src/main/java/nl/hva/vuwearable/MainActivity.kt
+++ app/src/main/java/nl/hva/vuwearable/MainActivity.kt
@@ -1,17 +1,26 @@
 package nl.hva.vuwearable
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.text.InputType
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import nl.hva.vuwearable.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var m_Text = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +40,41 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_button -> showCodeInputDialog()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showCodeInputDialog () {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.login_to_professor))
+
+        // Set up the input
+        val input = EditText(this)
+
+        // Specify the type of input expected
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(input)
+
+        // set buttons
+        builder.setPositiveButton(getString(R.string.login),
+            DialogInterface.OnClickListener { dialog, which -> checkInput(input.text.toString()) })
+        builder.setNegativeButton(getString(R.string.cancel),
+            DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+
+        builder.show()
+    }
+
+    private fun checkInput ( inputText : String) {
+        Toast.makeText(this, inputText, Toast.LENGTH_LONG).show()
     }
 }
