f28e8e00cb5005a344ab165f7836e027aa36e0da
svadkoc <SvadBellum1997!>
2022-12-13 11:15:22 +0100

    Splash: bugs

--- app/src/main/java/nl/hva/vuwearable/ui/SplashActivity.kt
+++ app/src/main/java/nl/hva/vuwearable/ui/SplashActivity.kt
@@ -2,6 +2,7 @@ package nl.hva.vuwearable.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import nl.hva.vuwearable.R
 import nl.hva.vuwearable.databinding.ActivitySplashscreenBinding
@@ -10,12 +11,27 @@ class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashscreenBinding
 
-    private val navController = findNavController(R.id.splashFragment2)
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_splashscreen)
+
+        navController = findNavController(R.id.nav_host_fragment_activity_splash)
+
+        setNavigation()
+    }
+
+    private fun setNavigation(){
+        binding.btnSetup.setOnClickListener{
+        }
+
+        binding.btnDashboard.setOnClickListener{
+            navController.navigate(R.id.nav_host_fragment_activity_main)
+        }
     }
 
 }
