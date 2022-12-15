aca9b33e466e2b0cbe8a3d0b5ffe95137908d31f
PhelixTaken <thegggameyt@gmail.com>
2022-12-15 11:04:01 +0100

    Breathing: Added listener to setup and showing breath in duration in seconds

--- app/src/main/java/nl/hva/vuwearable/ui/breathing/BreathingFragment.kt
+++ app/src/main/java/nl/hva/vuwearable/ui/breathing/BreathingFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -31,6 +33,18 @@ class BreathingFragment : Fragment() {
             getSeekBarData()
             findNavController().navigate(R.id.action_breathingFragment_to_excerciseFragment)
         }
+
+        binding.progressBreathInSeconds.text =
+            getString(R.string.seconds, binding.seekbarBreatheIn.progress)
+
+        binding.seekbarBreatheIn.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                binding.progressBreathInSeconds.text = getString(R.string.seconds, progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
 
         return root
     }
