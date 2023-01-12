3ebc132d40cfd372c01e768373bfa141835ba950
PhelixTaken <thegggameyt@gmail.com>
2023-01-12 10:38:24 +0100

    Dashboard: Wifi showing the right status

--- app/src/main/java/nl/hva/vuwearable/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/nl/hva/vuwearable/ui/dashboard/DashboardFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import nl.hva.vuwearable.R
 import nl.hva.vuwearable.databinding.FragmentDashboardBinding
 import nl.hva.vuwearable.ui.udp.UDPViewModel
 
@@ -46,19 +47,19 @@ class DashboardFragment : Fragment() {
         udpViewModel.isConnected.observe(viewLifecycleOwner) {
             when (it) {
                 true -> {
-//                    binding.ivWifi.setImageResource(R.drawable.ic_baseline_wifi_24)
-//                    binding.wifiConnection.text = getString(R.string.connection_success)
+                    binding.wifiIconImageview.setImageResource(R.drawable.wifi_icon)
+                    binding.wifiSubTextview.text = getString(R.string.connection_success)
 
                     udpViewModel.isReceivingData.observe(viewLifecycleOwner) { isReceivingData ->
                         if (!isReceivingData) {
-//                            binding.wifiConnection.text = getString(R.string.no_data_connection)
-//                            binding.ivWifi.setImageResource(R.drawable.ic_baseline_wifi_24_no_data)
+                            binding.wifiSubTextview.text = getString(R.string.no_data_connection)
+                            binding.wifiIconImageview.setImageResource(R.drawable.wifi_not_receiving_icon)
                         }
                     }
                 }
                 false -> {
-//                    binding.ivWifi.setImageResource(R.drawable.ic_baseline_wifi_off_24)
-//                    binding.wifiConnection.text = getString(R.string.connection_failed)
+                    binding.wifiIconImageview.setImageResource(R.drawable.wifi_off_icon)
+                    binding.wifiSubTextview.text = getString(R.string.connection_failed)
                 }
             }
         }
