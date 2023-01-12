ecfe0b263884c13ec9ebd9e6249839bcb1a56ac8
PhelixTaken <thegggameyt@gmail.com>
2023-01-12 12:38:11 +0100

    System: Making the measurement start stop group invisible when not logged in

--- app/src/main/java/nl/hva/vuwearable/ui/system/SystemFragment.kt
+++ app/src/main/java/nl/hva/vuwearable/ui/system/SystemFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import nl.hva.vuwearable.databinding.FragmentSystemBinding
+import nl.hva.vuwearable.ui.login.LoginViewModel
 
 class SystemFragment : Fragment() {
 
     private var _binding: FragmentSystemBinding? = null
 
     private val binding get() = _binding!!
+
+    private val loginViewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -20,6 +24,9 @@ class SystemFragment : Fragment() {
         _binding = FragmentSystemBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        if (!loginViewModel.checkIfUserIsLoggedIn()) {
+            binding.measurementGroup.visibility = View.INVISIBLE
+        }
 
         return root
     }
